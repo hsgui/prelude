2eaf8bac93afe4dfd40500b723e6951e896ba0c2
Shuigui Huang <shuhua@microsoft.com>
2015-01-07 10:36:24 +0800

    find the custom variable prelude-whitespace to turn off whitespace

--- personal/custom-theme.el
+++ personal/custom-theme.el
@@ -11,4 +11,9 @@
 ;; (add-hook 'text-mode-hook 'prelude-enable-wihtespace) makes the text mode enable whitespace minor mode
 ;; and org-mode is derived from text mode, so the line-column is 80
 ;; but this makes the Org files urgly!
-(setq whitespace-line-column 512) ;; no line length limit
+;(setq whitespace-line-column 512) ;; no line length limit
+;(setq whitespace-style nil) ;; I don't like the look.
+
+;; prelude-whitespace is defined in prelude-custom.el
+(custom-set-variables
+ '(prelude-whitespace nil))
